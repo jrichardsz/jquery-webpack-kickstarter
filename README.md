a55babd3863e19f0a96c6f3603334097a06b42f2
jrichardsz <jrichardsz.java@gmail.com>
2018-01-01 01:44:16 -0500

    Repository Inicialization

---
+++ README.md
@@ -0,0 +1,32 @@
+# jquery webpack kickstarter
+
+A basic jquery & webpack starter kit.
+
+This demo shows how webpack works with sass and third-party library(jQuery)
+
+How is it work?
+**install the deps**
+
+```bash
+npm install
+```
+
+**run the dev server**
+```bash
+npm run start
+```
+
+then open your http://localhost:8080 see how the magic happens.
+
+**build**
+
+```bash
+npm run build
+```
+then you'll find a new build folder generated.
+
+**TODO: testing**
+
+```bash
+npm run test
+```
---
+++ app/index.js
@@ -0,0 +1,35 @@
+import './main.scss';
+/*
+ * jquery plugin
+ * $,jQuery,window.jQuery webpack.config.js
+ * webpack.ProvidePlugin
+ * imports-loader,require,
+ * jQuery plugin.js
+ * config.js中module.loaders
+*/
+
+//webpack.config.js
+
+import $ from 'jquery';
+import ListModel from './ListModel';
+import ListView from './ListView';
+import ListController from './ListController';
+import Event from './Event';
+
+
+// example from http://jsfiddle.net/alex_netkachov/ZgBrK/
+// by Alex Netkachov
+
+$(document).ready(function() {
+
+  var model = new ListModel(['Java']),
+      view = new ListView(model, {
+          'list': $('#list'),
+          'addButton': $('#plusBtn'),
+          'delButton': $('#minusBtn')
+      }),
+      controller = new ListController(model, view);
+
+  view.show();
+
+});
